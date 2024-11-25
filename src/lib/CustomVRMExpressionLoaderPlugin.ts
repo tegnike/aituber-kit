3b6cb247f8c7a25491740476d219a5a94a9beb6a
tegnike <o3nike.teg.14@gmail.com>
2024-11-25 18:24:19 +0100

    Dify URLの指定方法を修正

---
+++ src/lib/CustomVRMExpressionLoaderPlugin.ts
@@ -0,0 +1,14 @@
+// import { VRMExpressionLoaderPlugin } from '@pixiv/three-vrm'
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+// export class CustomVRMExpressionLoaderPlugin extends VRMExpressionLoaderPlugin {
+//   constructor(parser: GLTFLoader) {
+//     super(parser)
+//     // v0v1PresetNameMap をカスタマイズ
+//     this.v0v1PresetNameMap = {
+//       ...this.v0v1PresetNameMap,
+//       Neutral2: 'relaxed', // 'fun' を 'relaxed' にマッピング
+//       // 他のマッピングもここで定義可能
+//     }
+//   }
+// }
