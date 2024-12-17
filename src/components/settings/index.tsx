51fdcf05998e91bf1abf8f52bc63451477c8293a
tegnike <o3nike.teg.14@gmail.com>
2024-12-17 12:28:52 +0100

    バージョン更新

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -126,7 +126,7 @@ const Main = () => {
 const Footer = () => {
   return (
     <footer className="absolute py-4 bg-[#413D43] text-center text-white font-Montserrat bottom-0 w-full">
-      powered by ChatVRM from Pixiv. version 2.18.0
+      powered by ChatVRM from Pixiv. version 2.18.1
     </footer>
   )
 }
