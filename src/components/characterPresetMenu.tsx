6bfb19a0cae7e9ef2e2a05397564e04640282646
tegnike <o3nike.teg.14@gmail.com>
2025-03-20 14:19:23 +0100

    プリセットボタンの位置調整

--- src/components/characterPresetMenu.tsx
+++ src/components/characterPresetMenu.tsx
@@ -78,7 +78,7 @@ const CharacterPresetMenu = () => {
   }
 
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-20 right-4 z-30">
       {/* メインボタン */}
       <button
         onClick={() => setIsOpen(!isOpen)}
