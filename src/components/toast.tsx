64f74fa8838dfd052d53bbea9f51755bf3428875
tegnike <o3nike.teg.14@gmail.com>
2025-04-17 00:13:43 +0200

    トーストコンポーネントに新たに「tool」タイプを追加

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next'
 
 type ToastProps = {
   message: string
-  type: 'success' | 'error' | 'info'
+  type: 'success' | 'error' | 'info' | 'tool'
   onClose: () => void
   duration?: number
   closing?: boolean
