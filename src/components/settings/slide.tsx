8fb73e4fc9872b14c0f678dc4aed263a8f545448
ニケちゃん / nikechan <o3nike.teg.14@gmail.com>
2025-04-23 20:21:02 +0200

    別の画面を開くsvgを適切に配置

--- src/components/settings/slide.tsx
+++ src/components/settings/slide.tsx
@@ -115,21 +115,13 @@ const Slide = () => {
                   className="inline-flex items-center px-3 py-2 text-sm bg-primary hover:bg-primary-hover rounded-3xl text-white font-bold transition-colors duration-200 whitespace-nowrap"
                 >
                   {t('EditSlideScripts')}
-                  {/* 外部リンクアイコンを追加しても良いかも */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 ml-1"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    />
-                  </svg>
+                  <Image
+                    src="/images/icons/external-link.svg"
+                    alt={t('OpenInNewTab')}
+                    width={16}
+                    height={16}
+                    className="ml-1"
+                  />
                 </a>
               </Link>
             )}
