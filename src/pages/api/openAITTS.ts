d03a56468ff9063ea127f4ef243a2d82bdb5ea97
SATOH Kiyoshi <satoh@hakuba.jp>
2025-05-06 17:29:37 +0900

    OPENAI_TTS_KEYにすべきところのtypoを修正

--- src/pages/api/openAITTS.ts
+++ src/pages/api/openAITTS.ts
@@ -11,7 +11,7 @@ export default async function handler(
 
   const { message, voice, model, speed, apiKey, emotion } = req.body
   const openaiKey =
-    apiKey || process.env.OPENAI_KEY || process.env.OPENAI_API_KEY
+    apiKey || process.env.OPENAI_TTS_KEY || process.env.OPENAI_API_KEY
 
   if (!message || !voice || !model || !openaiKey) {
     return res.status(400).json({ error: 'Missing required parameters' })
