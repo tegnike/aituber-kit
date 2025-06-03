135cf6ef086b9ddaac269cdd2984eb4441443ce1
ニケちゃん / nikechan <o3nike.teg.14@gmail.com>
2025-06-03 07:02:41 +0200

    xAIでもマルチモーダル対応モデルを表示

--- src/components/settings/modelProvider.tsx
+++ src/components/settings/modelProvider.tsx
@@ -788,7 +788,7 @@ const ModelProvider = () => {
                 >
                   {getModels('xai').map((model) => (
                     <option key={model} value={model}>
-                      {model}
+                      {model} {isMultiModalModel('xai', model) ? '📷' : ''}
                     </option>
                   ))}
                 </select>
