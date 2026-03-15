b0b281adfa7406ee1a0e66bb28edad00dcf0f8fa
ニケちゃん / nikechan <o3nike.teg.14@gmail.com>
2026-03-15 22:07:26 +0100

    feat: サイトURLを環境変数に追加し、メタ情報に反映

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,10 +1,10 @@
-import { buildUrl } from '@/utils/buildUrl'
 import Head from 'next/head'
 export const Meta = () => {
   const title = 'AITuberKit'
   const description =
     'Webブラウザだけで誰でも簡単にAIキャラと会話したり、Youtubeで配信したりできます。'
-  const imageUrl = '/ogp.png'
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://aituberkit.com'
+  const imageUrl = `${siteUrl}/ogp.png`
   return (
     <Head>
       <title>{title}</title>
@@ -12,6 +12,8 @@ export const Meta = () => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={imageUrl} />
+      <meta property="og:type" content="website" />
+      {siteUrl && <meta property="og:url" content={siteUrl} />}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
