81b66c3064faa6bdb94887aefb71a1987bd29779
tegnike <o3nike.teg.14@gmail.com>
2026-07-29 12:57:43 +0200

    Bump footer version to 2.63.0

--- src/components/settings/shell/Footer.tsx
+++ src/components/settings/shell/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer className="theme-surface-contrast shrink-0 border-t border-primary/20 py-1 text-center font-Montserrat text-xs">
-      powered by ChatVRM from Pixiv / ver. 2.62.0
+      powered by ChatVRM from Pixiv / ver. 2.63.0
     </footer>
   )
 }
